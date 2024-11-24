08e995b4a4aa8223324804366aa8b412e8ff78c9
kram <kram@allegion.com>
2024-11-24 16:13:19 +0530

    Initial Commit

---
+++ SWIFTUI WEATHER/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SWIFTUI WEATHER
+//
+//  Created by Ram, Keshaw on 24/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SWIFTUI WEATHER/SWIFTUI_WEATHERApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTUI_WEATHERApp.swift
+//  SWIFTUI WEATHER
+//
+//  Created by Ram, Keshaw on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUI_WEATHERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
